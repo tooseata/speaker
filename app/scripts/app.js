578c13e3959dc14df39fe375faeab290a7c80db0
palimpsests <zakzibrat@gmail.com>
2013-08-16 12:49:04 -0700

    Added Angular framework

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('speakerApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
