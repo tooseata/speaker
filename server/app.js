6f66768c959be82c5938b9a84ab6dffe4531a4d9
palimpsests <zakzibrat@gmail.com>
2013-08-17 15:47:40 -0700

    Fixed paths to app

--- server/app.js
+++ server/app.js
@@ -3,23 +3,23 @@ var express = require('express'),
   path = require('path'),
   io = require('socket.io'),
   appConfig = require( './../app-config.json' );
-  
+
   var app = express();
 
 // require('./config/middleware.js')(app);
 
 
 var config = module.exports = {};
-config.server = {'distFolder': path.resolve(__dirname, '../client/dist')};
+config.server = {'distFolder': path.resolve(__dirname, '../dist')};
 config.server = {'staticUrl': __dirname +'/static'};
 
 app.configure(function(){
-  app.set( 'views', path.join( __dirname, './../client/app' ) );
+  app.set( 'views', path.join( __dirname, './../app' ) );
   app.set( 'view engine', 'html' );
   app.set('port', process.env.PORT || 3000);
   app.use(express.bodyParser());
-  app.use(express.favicon(path.join( __dirname, './../client/app/favicon.ico')));
-  app.use( express.static( path.join( __dirname, './../client/app' ) ) );
+  app.use(express.favicon(path.join( __dirname, './../app/favicon.ico')));
+  app.use( express.static( path.join( __dirname, './../app' ) ) );
   app.use(app.router);
 
 });
