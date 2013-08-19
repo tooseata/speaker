12d32492365cf46569d1297e2a01460666786f57
palimpsests <zakzibrat@gmail.com>
2013-08-19 10:43:05 -0700

    Refactored karma-e2e configuration file

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,58 +1,73 @@
 // Karma E2E configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+'use strict';
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js'
-];
+module.exports = function (config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
+    frameworks: ['ng-scenario'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // list of files / patterns to load in the browser
+    files: [
+      ANGULAR_SCENARIO,
+      ANGULAR_SCENARIO_ADAPTER,
+      'test/e2e/**/*.js'
+    ],
 
-// web server port
-port = 8080;
+    urlRoot: '/_karma_/',
+    proxies: {
+      '/': 'http://localhost:8000/'
+    },
 
-// cli runner port
-runnerPort = 9100;
+    // list of files to exclude
+    exclude: [],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress'],
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // web server port
+    port: 8080,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // cli runner port
+    runnerPort: 9100,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// Uncomment the following lines if you are using grunt's server to run the tests
-// proxies = {
-//   '/': 'http://localhost:9000/'
-// };
-// URL root prevent conflicts with the site root
-// urlRoot = '_karma_';
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies = {
+    //   '/': 'http://localhost:9000/'
+    // };
+    // URL root prevent conflicts with the site root
+    // urlRoot = '_karma_';
+
+
+  });
+};
