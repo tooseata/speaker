45eb507d6f4b2084202963e641ce777b8f7f2794
Hack Reactor Students <github@hackreactor.com>
2013-08-19 11:09:28 -0700

    Users joining will increment member count.

--- client/scripts/controllers/admin.js
+++ client/scripts/controllers/admin.js
@@ -3,12 +3,18 @@
 angular.module('speakerApp')
   .controller('AdminCtrl', function ($scope, User, socket) {
     $scope.user = User.get();
-    $scope.members = '100';
+    $scope.members = 1;
     $scope.talkRequests = {};
     socket.on('new:talkRequest', function (user) {
       $scope.talkRequests[user.name] = user;
     });
     socket.on('new:cancelTalkRequest', function (user) {
       delete $scope.talkRequests[user.name];
+    });
+    socket.on('new:leaveRoom', function (user) {
+      $scope.members--;
+    });
+    socket.on('new:joinRoom', function (user) {
+      $scope.members++;
     });
   });
