7c3987f6853793659a24c9618afb412bd26cee74
Hack Reactor Students <github@hackreactor.com>
2013-08-19 10:13:06 -0700

    Request button toggles. Server broadcasts a cancel request.

--- client/scripts/controllers/talk.js
+++ client/scripts/controllers/talk.js
@@ -3,7 +3,13 @@
 angular.module('speakerApp')
   .controller('TalkCtrl', function ($scope, User, socket) {
     $scope.user = User;
+    $scope.sentRequest = false;
     $scope.sendTalkRequest = function(){
       socket.emit('broadcast:talkRequest', {user : $scope.user.get()});
+      $scope.sentRequest = true;
+    };
+    $scope.cancelTalkRequest = function(){
+      socket.emit('broadcast:cancelTalkRequest', {user : $scope.user.get()});
+      $scope.sentRequest = false;
     };
   });
