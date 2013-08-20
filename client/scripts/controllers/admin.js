3edec7475d8333d7d6e06b5d3f0cb43b634151e1
Hack Reactor Students <github@hackreactor.com>
2013-08-20 12:12:04 -0700

    Allows admin to open and close request queue

--- client/scripts/controllers/admin.js
+++ client/scripts/controllers/admin.js
@@ -5,12 +5,6 @@ angular.module('speakerApp')
     $scope.user = User.get();
     $scope.members = 0;
     $scope.talkRequests = {};
-    socket.on('new:talkRequest', function (user) {
-      $scope.talkRequests[user.name] = user;
-    });
-    socket.on('new:cancelTalkRequest', function (user) {
-      delete $scope.talkRequests[user.name];
-    });
     socket.on('new:leaveRoom', function (user) {
       console.log('a user left the room');
       delete $scope.talkRequests[user.name];
@@ -19,4 +13,19 @@ angular.module('speakerApp')
     socket.on('new:joinRoom', function () {
       $scope.members++;
     });
+    $scope.queueStatus = false;
+    $scope.openQueue = function(){
+      $scope.queueStatus = true;
+      $scope.talkRequests = {};
+      socket.on('new:talkRequest', function (user) {
+        $scope.talkRequests[user.name] = user;
+      });
+      socket.on('new:cancelTalkRequest', function (user) {
+        delete $scope.talkRequests[user.name];
+      });
+    };
+    $scope.closeQueue = function(){
+      $scope.queueStatus = false;
+    };
+
   });
