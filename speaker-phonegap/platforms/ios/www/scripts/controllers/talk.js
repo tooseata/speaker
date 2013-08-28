8815e94a40ce606e808b9ae3e36464e367c52ccd
Hack Reactor Students <github@hackreactor.com>
2013-08-28 11:24:44 -0700

    Gutted WebRTC code; added Opentok framework

--- speaker-phonegap/platforms/ios/www/scripts/controllers/talk.js
+++ speaker-phonegap/platforms/ios/www/scripts/controllers/talk.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('speakerApp')
-.controller('TalkCtrl', function ($scope, $location, User, Session, socketService, socket, WebRtcService, $http, $window) {
+.controller('TalkCtrl', function ($scope, $location, User, Session, socketService, socket, $http, $window) {
             
             Session.user($scope);
             $scope.user = User.get();
@@ -45,132 +45,11 @@ angular.module('speakerApp')
             $scope.sentQuestion = true;
             };
             
-            $scope.requestVideo = function() {
-            console.log('trigger video');
-            WebRtcService.sendMessage({type: 'media type', value: 'video'});
-            var localVideo = document.querySelector('localVideo');
-            var constraints = {audio: true, video: true};
-            
-            var onStreamError = function(e) {
-            console.error('Error getting video', e);
-            };
-            
-            var onVideoStream = function(stream) {
-            socket.emit('broadcast:talkRequest', $scope.user);
-            $scope.sentRequest = true;
-            handleUserMedia(stream, {video: true});
-            };
-            
-            getUserMedia(constraints, onVideoStream, onStreamError);
-            };
-            
-            $scope.requestAudio = function(){
-            WebRtcService.sendMessage({type: 'media type', value: 'video'});
-            var MicrophoneSample = function() {
-            this._width = 640;
-            this._height = 480;
-            this.canvas = document.querySelector('canvas');
-            };
-            var sample = new MicrophoneSample();
-            var context = new webkitAudioContext();
-            var analyser = context.createAnalyser();
-            // shim layer with setTimeout fallback
-            var requestAnimFrame = (function(){
-                                    return  window.requestAnimationFrame       ||
-                                    window.webkitRequestAnimationFrame ||
-                                    window.mozRequestAnimationFrame    ||
-                                    window.oRequestAnimationFrame      ||
-                                    window.msRequestAnimationFrame     ||
-                                    function( callback ){
-                                    window.setTimeout(callback, 1000 / 60);
-                                    };
-                                    })();
-            
-            // getUserMedia(constraints, handleUserMedia, )
-            var getMicrophoneInput = function (source) {
-            getUserMedia({audio: true}, onStream, onStreamError);
-            };
-            var onStream = function(stream) {
-            socket.emit('broadcast:microphoneClickedOnClientSide');
-            var input = context.createMediaStreamSource(stream);
-            var filter = context.createBiquadFilter();
-            filter.frequency.value = 6600.0;
-            filter.type = filter.NOTCH;
-            filter.Q = 10.0;
-            // Connect graph.
-            input.connect(filter);
-            filter.connect(analyser);
-            requestAnimFrame(visualize.bind(analyser));
-            socket.emit('broadcast:talkRequest', $scope.user);
-            $scope.sentRequest = true;
-            handleUserMedia(stream);
-            };
-            
-            var onStreamError = function(e) {
-            console.error('Error getting microphone', e);
-            };
-            
-            var visualize = function() {
-            sample.canvas.width = sample._width;
-            sample.canvas.height = sample._height;
-            var drawContext = sample.canvas.getContext('2d');
-            
-            var times = new Uint8Array(analyser.frequencyBinCount);
-            analyser.getByteTimeDomainData(times);
-            for (var i = 0; i < times.length; i++) {
-            var value = times[i];
-            var percent = value / 256;
-            var height = sample._height * percent;
-            var offset = sample._height - height - 1;
-            var barWidth = sample._width/times.length;
-            drawContext.fillStyle = 'black';
-            drawContext.fillRect(i * barWidth, offset, 1, 1);
-            }
-            requestAnimFrame(visualize.bind(analyser));
-            };
-            
-            getMicrophoneInput(sample);
-            };
-            
-            var handleUserMedia = function(stream, type) {
-            console.log('handleUserMedia was called and passed', stream);
-            socketService.localStream = stream;
-            if (arguments[1]) {
-            attachMediaStream(localVideo, stream);
-            }
-            WebRtcService.sendMessage('got user media');
-            };
-            
-            socket.on('message', function(message) {
-                      console.log('Received message: ', message);
-                      if (message === 'got user media') {
-                      WebRtcService.maybeStart();
-                      } else if (message.type === 'offer') {
-                      console.log('received offer on client side');
-                      WebRtcService.maybeStart();
-                      socketService.pc.setRemoteDescription(new RTCSessionDescription(message));
-                      doAnswer();
-                      } else if (message.type === 'answer' && socketService.isStarted) {
-                      socketService.pc.setRemoteDescription(new RTCSessionDescription(message));
-                      } else if (message.type === 'candidate' && socketService.isStarted) {
-                      var candidate = new RTCIceCandidate({sdpMLineIndex:message.label,
-                                                          candidate:message.candidate});
-                      console.log('***candidate***: ', candidate);
-                      socketService.pc.addIceCandidate(candidate);
-                      } else if (message === 'bye' && socketService.isStarted) {
-                      WebRtcService.handleRemoteHangup();
-                      }
-                      });
-            
-            var doAnswer = function() {
-            console.log('Sending answer to peer.');
-            socketService.pc.createAnswer(WebRtcService.setLocalAndSendMessage, null, WebRtcService.sdpConstraints);
-            };
-            
-            WebRtcService.requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+           
+           
             
             $window.onbeforeunload = function(e) {
-            socket.emit('broadcast:cancelTalkRequest', $scope.user);
-            $scope.sentRequest = false;
-            };
+                socket.emit('broadcast:cancelTalkRequest', $scope.user);
+                $scope.sentRequest = false;
+                };
             });
