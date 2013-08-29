673e4e14003c177d76b15303400a323cd3fc19bd
Hack Reactor Students <github@hackreactor.com>
2013-08-29 16:42:19 -0700

    User can now just link to the room.

--- client/scripts/app/app.js
+++ client/scripts/app/app.js
@@ -5,36 +5,14 @@ var app = angular.module('speakerApp', ['ui.validate', 'ngCookies']);
 app.config(function ($routeProvider, $httpProvider) {
 
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/join', {
-        templateUrl: 'views/join.html',
-        controller: 'JoinCtrl'
-      })
-      .when('/create', {
-        templateUrl: 'views/create.html',
-        controller: 'CreateCtrl'
-      })
-      .when('/talk', {
-        templateUrl: 'views/talk.html',
-        controller: 'TalkCtrl'
-      })
-      .when('/admin', {
-        templateUrl: 'views/admin.html',
-        controller: 'AdminCtrl'
-      })
-      .when('/listen', {
-        templateUrl: 'views/listen.html',
-        controller: 'ListenCtrl'
-      })
-      .when('/questions', {
-        templateUrl: 'views/questions.html',
-        controller: 'QuestionsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+      .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
+      .when('/join', {templateUrl: 'views/join.html', controller: 'JoinCtrl'})
+      .when('/join/:roomName', {templateUrl: 'views/joinRoom.html', controller: 'JoinCtrl'})
+      .when('/create', {templateUrl: 'views/create.html', controller: 'CreateCtrl'})
+      .when('/talk', {templateUrl: 'views/talk.html', controller: 'TalkCtrl'})
+      .when('/admin', {templateUrl: 'views/admin.html', controller: 'AdminCtrl'})
+      .when('/listen', {templateUrl: 'views/listen.html', controller: 'ListenCtrl'})
+      .when('/questions', {templateUrl: 'views/questions.html', controller: 'QuestionsCtrl'})
+      .otherwise({redirectTo: '/'});
     $httpProvider.defaults.withCredentials = true;
   });
--- client/scripts/controllers/join.js
+++ client/scripts/controllers/join.js
@@ -4,14 +4,24 @@ angular.module('speakerApp')
   .controller('JoinCtrl', function ($scope, $location, $http, User, socket, Session) {
 
     // Scope variables.
-    Session.existingRooms($scope);
     $scope.user = User.get();
-
+    var path = $location.path().replace('/join', '').slice(1);
+    $http.get('/rooms').success(function(data){
+      $scope.existingRooms = data;
+      if (path && !data[path]){
+        $location.path('/join');
+      } else {
+        $scope.room = path;
+        User.setRoom(path);
+      }
+    }).error(function(){
+      console.log('error on room collection.');
+    });
     $scope.update = function(userName, room) {
       User.setType('user');
       User.setName(userName);
-      User.setRoom(room);
       $scope.user = User.get();
+      room && User.setRoom(room);
       socket.emit('broadcast:joinRoom', $scope.user);
       $http.post('/session', JSON.stringify($scope.user));
       $location.path('/talk');
