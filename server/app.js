2790076fa85c72b92d4fc238b0b6f93b4d88bde9
Chris Sita <csita@ourstage.com>
2013-08-30 11:34:07 -0700

    Changed CORS access countrol settings

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ var sessions = {};
 //     token;
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials', 'true');
