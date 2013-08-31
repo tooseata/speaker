a81db9c10ac1ef4145a34c7ce14c070a5e660424
krgeppert <krgeppert@gmail.com>
2013-08-31 14:16:07 -0700

    Added input validation to home page.

--- client/scripts/app/app.js
+++ client/scripts/app/app.js
@@ -5,8 +5,7 @@ var app = angular.module('speakerApp', ['ui.validate', 'ngCookies']);
   app.config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
-      .when('/join', {templateUrl: 'views/join.html', controller: 'JoinCtrl'})
-      .when('/join/:roomName', {templateUrl: 'views/joinRoom.html', controller: 'JoinCtrl'})
+      .when('/join/:roomName', {templateUrl: 'views/join.html', controller: 'JoinCtrl'})
       .when('/create', {templateUrl: 'views/create.html', controller: 'CreateCtrl'})
       .when('/talk', {templateUrl: 'views/talk.html', controller: 'TalkCtrl'})
       .when('/admin', {templateUrl: 'views/admin.html', controller: 'AdminCtrl'})
